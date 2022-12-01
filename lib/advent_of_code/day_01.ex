4ccd47c2aa4c679678492f4d292beead3697c491
Bobby McDonald <bobbymcwho@gmail.com>
2022-12-01 10:58:24 -0500

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,24 @@
 defmodule AdventOfCode.Day01 do
   def part1(_args) do
+    parse_input()
+    |> Enum.max()
   end
 
   def part2(_args) do
+    parse_input()
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  def parse_input() do
+    AdventOfCode.Input.get!(1)
+    |> String.trim()
+    |> String.split("\n\n")
+    |> Enum.map(fn string ->
+      String.split(string, "\n")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
   end
 end
